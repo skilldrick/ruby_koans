5a6f2d33c797b2d2ca4d7becaf80195e0597da25
skilldrick <skilldrick@gmail.com>
2010-11-13 13:10:16 +0000

    154/269

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a <= 0 || b <= 0 || c <=0
+    raise TriangleError, "All sides must have greater than zero length"
+  end
+
+  if (a + b <= c) || (b + c <= a) || (a + c <= b)
+    raise TriangleError, "Longest side must not be longer than the two other sides"
+  end
+
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
